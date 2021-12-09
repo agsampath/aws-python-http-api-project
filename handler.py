54924427af595685767b3a36c9a6eda766fa25b7
agsampath <ag.sampath@gmail.com>
2021-12-09 22:22:19 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -4,7 +4,6 @@ import json
 def hello(event, context):
     body = {
         "message": "Go Serverless v2.0! Your function executed successfully!",
-        "input": event,
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
